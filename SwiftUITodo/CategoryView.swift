d597e35aba09de748ab061831162155088a16d36
seiya endo <seiya.endo0830@gmail.com>
2021-05-27 16:14:43 +0900

    fix: error

---
+++ SwiftUITodo/CategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryView.swift
+//  SwiftUITodo
+//
+//  Created by 遠藤聖也 on 2021/05/27.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryView()
+    }
+}
