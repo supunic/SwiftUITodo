ebe7cc096b1f88751d0853025e366c1f06c494b8
seiya endo <seiya.endo0830@gmail.com>
2021-05-27 14:32:58 +0900

    update: checkbox to Binding

--- SwiftUITodo/CheckBox.swift
+++ SwiftUITodo/CheckBox.swift
@@ -9,19 +9,31 @@ import SwiftUI
 
 struct CheckBox: View {
     // @State → checkedというステータスをSwiftUIで管理するための宣言、これをつけることで変数の変更をリアクティブに画面に反映できる
-    @State var checked: Bool = false
+//    @State var checked: Bool = false
+    
+    // @Binding アノテーション → 親のviewと値を共有したいときに使用する
+    // .constant(false)でBinding型のBool値を定義できる
+    @Binding var checked: Bool
     var body: some View {
         // バインディング構造体 → $マークが必要
         // 値を複数のviewで共有するためのもの
         // 別のviewでcheckedが使える
-        Toggle(isOn: $checked) {
-            Text("チェックボックス")
-        }
+//        Toggle(isOn: $checked) {
+//            Text("チェックボックス")
+//        }
+        Image(systemName: checked ? "checkmark.circle" : "circle")
+            .onTapGesture {
+                self.checked.toggle()
+            }
     }
 }
 
+// PreviewProvider → Canvas へのプレビュー用のコード、実際のプログラムの実行には関係ない
 struct CheckBox_Previews: PreviewProvider {
     static var previews: some View {
-        CheckBox()
+        VStack {
+            CheckBox(checked: .constant(false))
+            CheckBox(checked: .constant(true))
+        }
     }
 }
